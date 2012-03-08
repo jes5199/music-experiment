7e33090d9a60108129c9146c753b66d9cc008d3e
Jesse Wolfe <jes5199@gmail.com>
2012-03-07 20:30:22 -0800

    i dont understand scaling

--- compare.rb
+++ compare.rb
@@ -31,12 +31,16 @@ def delay_fft( delay, fft_data )
   return r
 end
 
-def fft_to_file( fft, filename, size, info )
+def fft_to_file( fft, filename, size, info, scale )
   result_data = FFTW3.ifft( fft )
+  data_to_file( result_data, filename, size, info, scale )
+end
+
+def data_to_file( data, filename, size, info, scale )
   result_buffer = RubyAudio::Buffer.new("float", size, 1)
   i = 0
-  result_data.each do |r|
-    result_buffer[i] = r.real
+  data.each do |r|
+    result_buffer[i] = (r.respond_to?(:real) ? r.real : r.to_f) / scale
     i += 1
   end
   output = RubyAudio::Sound.new(filename, "w", info)
@@ -58,8 +62,13 @@ sample_data[target_data.real_size - 1] = 0
 target_fft = FFTW3.fft( target_data.entries )
 sample_fft = FFTW3.fft( sample_data.entries )
 
-fft_to_file( FFTW3.ifft(target_fft), "Amen.ifft.wav", target_data.size, target.info )
-raise "what"
+ifft_target_fft = FFTW3.ifft(target_fft)
+(0...ifft_target_fft.size).each do |i|
+  p([ target_data[i], (ifft_target_fft[i].real / 308112.0)]) if (target_data[i] - (ifft_target_fft[i].real / 308112.0)).abs > 0.00000001
+end
+
+#data_to_file( target_data, "Amen.ifft.wav", target_data.size, target.info, 308112.0 )
+data_to_file( ifft_target_fft, "Amen.ifft.wav", target_data.size, target.info, 308112.0 )
 
 result_fft = NArray.new("complex", target_fft.size)
 
