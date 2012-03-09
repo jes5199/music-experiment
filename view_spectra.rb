3e7206214f4c20cf709172a6009557194a646adb
Jesse Wolfe <jes5199@gmail.com>
2012-03-09 14:32:58 -0800

    rgb

--- view_spectra.rb
+++ view_spectra.rb
@@ -5,19 +5,22 @@ scaling = 1.0
 
 file_size = File.size(spectra_filename)
 n = 100
-puts "P2"
+puts "P3"
 File.open(spectra_filename) do |f|
   frame_length = f.read(64 / 8).unpack("Q").first
   height = ( (file_size / (64/8)) - 1 ) / frame_length
   puts "#{frame_length} #{height}"
-  puts 256
+  puts 255
   while ! f.eof?
     frame = f.read(frame_length * 64 / 8).unpack("D*")
     frame.each do |val|
-      grey = (((val * scaling) + 1) * 127).to_i
-      grey = [grey, 255].min
-      grey = [grey, 0].max
-      print("%-4d" % grey)
+      value = (((val * scaling) + 1) * (2**(8*3 - 1)) ).to_i
+      red = value % 256
+      green = ( value / 256 ) % 256
+      blue = ( value / (256 ** 2) ) % 256
+      [red, green, blue].each do |v|
+        print("%-4d" % v)
+      end
     end
     puts
   end
