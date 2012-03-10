13716613b06a51e79143920ea2740f221e1fdff0
Jesse Wolfe <jes5199@gmail.com>
2012-03-10 13:36:24 -0800

    move images into a directory

--- show_spectra_amen.sh
+++
@@ -1,1 +0,0 @@
-ruby spectra.rb AmenMono.wav && ruby view_spectra.rb spectra/AmenMono.wav.spectra > spectra.pgm && convert spectra.pgm spectra.png && open spectra.png 
--- show_spectra_benchmark.sh
+++ show_spectra_benchmark.sh
@@ -1,1 +1,1 @@
-ruby spectra.rb sygyt.aiff && ruby view_spectra.rb spectra/sygyt.aiff.spectra > spectra.benchmark.pgm && convert spectra.benchmark.pgm spectra.benchmark.png && open spectra.benchmark.png 
+bash show_spectra.sh sygyt.aiff
