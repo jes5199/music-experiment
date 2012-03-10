e4f9ed0f0215356857fa1013360e0289e795e094
Jesse Wolfe <jes5199@gmail.com>
2012-03-10 13:37:10 -0800

    mkdir

--- show_spectra.sh
+++ show_spectra.sh
@@ -1,1 +1,1 @@
-ruby spectra.rb $1 && ruby view_spectra.rb spectra/$1.spectra > images/$1.pgm && convert images/$1.pgm images/$1.png && open images/$1.png 
+mkdir -p images && ruby spectra.rb $1 && ruby view_spectra.rb spectra/$1.spectra > images/$1.pgm && convert images/$1.pgm images/$1.png && open images/$1.png 
