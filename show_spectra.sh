fb12adcace5427d4128e58e2582dbb1ec6998edc
Jesse Wolfe <jes5199@gmail.com>
2012-03-11 16:43:10 -0700

    horizontal

--- show_spectra.sh
+++ show_spectra.sh
@@ -1,1 +1,1 @@
-mkdir -p images && ruby spectra.rb $1 && ruby view_spectra.rb spectra/$1.spectra > images/$1.pgm && convert images/$1.pgm images/$1.png && open images/$1.png 
+mkdir -p images && ruby spectra.rb $1 && ruby view_spectra.rb spectra/$1.spectra > images/$1.pgm && convert images/$1.pgm -flip -transverse images/$1.png && open images/$1.png 
