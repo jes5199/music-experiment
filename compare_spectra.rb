be7f4d3127212965b25a1ff724a6a62ee5a23afb
Jesse Wolfe <jes5199@gmail.com>
2012-03-11 21:55:18 -0700

    this would probably work slowly

--- compare_spectra.rb
+++ compare_spectra.rb
@@ -44,6 +44,45 @@ def fps_for_soundfile(filename)
   return frames_per_second
 end
 
+def delay_fft( delay, fft_data )
+  r = NArray.new("complex", fft_data.size)
+
+  _D = delay
+  _N = fft_data.size
+  k = 0 # fft frame number
+
+  fft_data.each do |f|
+    r[k] = f * ( Math::E ** Complex(0, (-2 * Math::PI * k * _D / _N)) )
+    k += 1
+  end
+
+  return r
+end
+
+def fft_to_file( fft, filename, size, info )
+  result_data = FFTW3.ifft( fft )
+  data_to_file( result_data, filename, size, info, result_data.size )
+end
+
+def data_to_file( data, filename, size, info, scale )
+  result_buffer = RubyAudio::Buffer.new("float", size, 1)
+  i = 0
+  data.each do |r|
+    result_buffer[i] = (r.respond_to?(:real) ? r.real : r.to_f) / scale
+    i += 1
+  end
+  output = RubyAudio::Sound.new(filename, "w", info)
+  output.write(result_buffer)
+  output.close
+end
+
+def save_params_for(sound_filename)
+  sound = RubyAudio::Sound.open(sound_filename)
+  frames_per_second = sound.info.samplerate
+  target_data = sound.read(:float, frames_per_second * 20)
+  [target_data.size, sound.info]
+end
+
 target_filename = "AmenMono.wav"
 frames_per_second = frames_per_second_for_sound(target_filename)
 target_spectra = spectra_for_soundfile(target_filename)
@@ -56,11 +95,39 @@ sample_fft = FFTW3.fft( sample_spectra )
 
 heap = Containers::MaxHeap.new
 corr = FFTW3.ifft(target_fft * sample_fft.conj).real
+corr_flat = []
 (0...corr.sizes[1]).each do |i|
-  heap.push [ corr[i * corr.sizes[0]], i]
+  val = corr[i * corr.sizes[0]]
+  heap.push [ val, i]
+  corr_flat << val
 end
 
+steps_per_second = (frames_per_second.to_f / FRAME_STEPS)
+
 score, index = heap.max
-time = (index * FRAME_STEPS) / frames_per_second.to_f
+time = index / steps_per_second
 
+peaks = []
+window_size =  steps_per_second / 20
+blanks = [0] * (window_size / 2)
+index = 0
+(blanks + corr_flat + blanks).each_cons(window_size) do |window|
+  if window[ window_size / 2 ] == window.max
+    raise "oops" if window[ window_size / 2 ] != corr_flat[index]
+    peaks << index
+  end
+  index += 1
+end
 
+result_fft = NArray.new("complex", target_fft.size)
+peaks.each do |offset|
+  time = offset / steps_per_second
+  p time
+
+  delayed_sample_fft = delay_fft( offset * FRAME_STEPS, sample_fft )
+  result_fft += delayed_sample_fft
+
+  fft_to_file( result_fft, "output.wav", *save_params_for(target_filename))
+end
+
+fft_to_file( result_fft, "output.wav", *save_params_for(target_filename))
