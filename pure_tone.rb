c111b4805d6e267c8c1fa545bd2b4a9b57f9b79b
Jesse Wolfe <jes5199@gmail.com>
2012-03-25 12:29:19 -0700

    tweaks

--- pure_tone.rb
+++ pure_tone.rb
@@ -16,6 +16,10 @@ def make_tone( frequency = 440.0, duration = 1.0, frames_per_second = 44100 )
   end
 end
 
+def duration_of_one_beat_at_bpm(n = 140)
+  60 / n.to_f
+end
+
 # 0 A
 # 1 A#
 # 2 B
@@ -29,14 +33,14 @@ end
 # 10 G
 # 11 G#
 
-c_major_scale = [3,5,7,8,10,12,14,15]
+c_major_scale = [-9,-7,-5,-4,-2,0,2,3]
 
 result_buffer = RubyAudio::Buffer.new("float", 44100 * 13, 1)
 i = 0
 c_major_scale.each do |note|
-  f = 220 * (2 ** (note/12.0))
+  f = 440 * (2 ** (note/12.0))
   p f
-  make_tone(f) do |val|
+  make_tone(f, duration_of_one_beat_at_bpm(140)) do |val|
     result_buffer[i] = val
     i += 1
   end
